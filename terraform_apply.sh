41bcb54736316e7d388ed59ee74e95b315e0610f
Esquive <e.falk@niugroup.lu>
2022-09-30 14:00:44 +0200

    changes in the subnet config.

--- terraform_apply.sh
+++ terraform_apply.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-terraform apply -var="privSubnetId=subnet-0baa60163f5fb38d7" \
--var="pubSubnetId=subnet-069f3b85f5328bbc2" \
--var="vpcId=vpc-041189264a99a503c" \
--var="securityGroupId=sg-0db74a76f9b42afeb" \
--var="ecr_ipfs-image=867912683733.dkr.ecr.eu-central-1.amazonaws.com/filedgr-ipfs:build" \
+terraform apply -var="privSubnetId=${PRIV_SUBNETID}" \
+-var="pubSubnetId=${PRIV_SUBNETID}" \
+-var="vpcId=${VPCID}" \
+-var="securityGroupId=${SGID}" \
+-var="ecr_ipfs-image=${IMAGE_ID}" \
 --auto-approve
